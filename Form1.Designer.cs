b244301c5a6426168c9e088400f7cdc07fb2dec1
mohammedMesbahi <mohammed.mesbahiaouam@usmba.ac.ma>
2022-11-28 13:07:21 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace EXAMENATION_SYSTEM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelectUser = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.wrongLabel = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.checkBoxShowHide = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStudentLogin = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEnrollmentNo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnStudentRegister = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quiz / exam managment system";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(441, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select user Type";
+            // 
+            // txtSelectUser
+            // 
+            this.txtSelectUser.FormattingEnabled = true;
+            this.txtSelectUser.Items.AddRange(new object[] {
+            "Student",
+            "Teacher"});
+            this.txtSelectUser.Location = new System.Drawing.Point(302, 200);
+            this.txtSelectUser.Name = "txtSelectUser";
+            this.txtSelectUser.Size = new System.Drawing.Size(427, 28);
+            this.txtSelectUser.TabIndex = 2;
+            this.txtSelectUser.SelectedIndexChanged += new System.EventHandler(this.txtSelectUser_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBoxShowHide);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.wrongLabel);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.txtUserName);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(267, 278);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(480, 391);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Teacher login";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(215, 96);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(165, 26);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(215, 173);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(165, 26);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "userName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // wrongLabel
+            // 
+            this.wrongLabel.AutoSize = true;
+            this.wrongLabel.Location = new System.Drawing.Point(127, 244);
+            this.wrongLabel.Name = "wrongLabel";
+            this.wrongLabel.Size = new System.Drawing.Size(226, 20);
+            this.wrongLabel.TabIndex = 5;
+            this.wrongLabel.Text = "wrogn usernae \"OR\" password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(121, 291);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(248, 41);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "sign in ";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // checkBoxShowHide
+            // 
+            this.checkBoxShowHide.AutoSize = true;
+            this.checkBoxShowHide.Location = new System.Drawing.Point(225, 217);
+            this.checkBoxShowHide.Name = "checkBoxShowHide";
+            this.checkBoxShowHide.Size = new System.Drawing.Size(144, 24);
+            this.checkBoxShowHide.TabIndex = 7;
+            this.checkBoxShowHide.Text = "show password";
+            this.checkBoxShowHide.UseVisualStyleBackColor = true;
+            this.checkBoxShowHide.CheckedChanged += new System.EventHandler(this.checkBoxShow_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnStudentRegister);
+            this.panel2.Controls.Add(this.btnStudentLogin);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.txtEnrollmentNo);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Location = new System.Drawing.Point(270, 278);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(480, 391);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnStudentLogin
+            // 
+            this.btnStudentLogin.Location = new System.Drawing.Point(126, 228);
+            this.btnStudentLogin.Name = "btnStudentLogin";
+            this.btnStudentLogin.Size = new System.Drawing.Size(248, 41);
+            this.btnStudentLogin.TabIndex = 6;
+            this.btnStudentLogin.Text = "log in ";
+            this.btnStudentLogin.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(146, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(215, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Student Enrollement Number";
+            // 
+            // txtEnrollmentNo
+            // 
+            this.txtEnrollmentNo.Location = new System.Drawing.Point(75, 175);
+            this.txtEnrollmentNo.Name = "txtEnrollmentNo";
+            this.txtEnrollmentNo.Size = new System.Drawing.Size(346, 26);
+            this.txtEnrollmentNo.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(197, 43);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 20);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Student Login";
+            // 
+            // btnStudentRegister
+            // 
+            this.btnStudentRegister.Location = new System.Drawing.Point(126, 290);
+            this.btnStudentRegister.Name = "btnStudentRegister";
+            this.btnStudentRegister.Size = new System.Drawing.Size(248, 41);
+            this.btnStudentRegister.TabIndex = 7;
+            this.btnStudentRegister.Text = "register";
+            this.btnStudentRegister.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 768);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtSelectUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtSelectUser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBoxShowHide;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label wrongLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStudentRegister;
+        private System.Windows.Forms.Button btnStudentLogin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEnrollmentNo;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXAMENATION_SYSTEM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            wrongLabel.Visible = false;
+        }
+
+        private void txtSelectUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(txtSelectUser.SelectedIndex == 0)
+            {
+                panel2.Visible = true;
+                panel1.Visible = false;
+
+            }
+            else if(txtSelectUser.SelectedIndex == 1)
+            {
+                panel2.Visible = false;
+                panel1.Visible = true;
+            }
+        }
+
+        private void checkBoxShow_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxShowHide.Checked == true)
+            {
+                txtPassword.PasswordChar = '\0';
+                checkBoxShowHide.Text = "Hide password";
+
+            }
+            else
+            {
+                txtPassword.PasswordChar = '*';
+                checkBoxShowHide.Text = "Show password";
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUserName.Text == "btechdays" && txtPassword.Text == "btechdays")
+            {
+                wrongLabel.Visible = false;
+                Teacher te = new Teacher();
+                te.Show();
+                this.Hide();
+            }
+            else
+            {
+                wrongLabel.Visible = true;
+            }
+        }
+    }
+}
